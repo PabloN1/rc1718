364a53e1ff7425ac3db9333f6385266096878094
PabloN1 <pablonuno.nobrega@alu.uhu.es>
2018-03-13 11:27:12 +0100

    Peano a Decimal y Decimal a Peano

--- Decimal2peano.pl
+++ Decimal2peano.pl
@@ -1,5 +1,8 @@
 %dec2peano(+Dec, -Peano)
 %es cierto cuando peano unifica con el numero natural equivalente a al Decimal Dec
 %ejemplo
-%dec2peano(3,Peano)
-%Peano= s(s(s(0)))
+%dec2peano(3,P)
+%P= s(s(s(0)))
+
+dec2peano(0,0).
+dec2peano(X,s(Y)):- N is X-1, dec2peano(N,Y).
---
+++ Peano2Decimal.pl
@@ -0,0 +1,2 @@
+peano2dec(0,0).
+peano2dec(s(X),Y):- peano2dec(X,N), Y is N+1.
